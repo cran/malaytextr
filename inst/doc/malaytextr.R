3dd9f0da507ac185f7c4be142b12d65c6d47844e
Zahier Nasrudin <zahiernasrudin@gmail.com>
2021-08-16 07:00:02 +0000

    version 0.1.0

---
+++ inst/doc/malaytextr.R
@@ -0,0 +1,37 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(malaytextr)
+
+## -----------------------------------------------------------------------------
+
+head(malayrootwords)
+
+
+## -----------------------------------------------------------------------------
+
+stem_malay(Word = "banyaknya", dictionary = malayrootwords)
+
+
+## -----------------------------------------------------------------------------
+
+x <- data.frame(text = c("banyaknya","sangat","terkedu", "pengetahuan"))
+
+stem_malay(Word = x, 
+          dictionary = malayrootwords, 
+          col_feature1 = "text")
+
+
+
+## -----------------------------------------------------------------------------
+
+x <- c("test https://t.co/fkQC2dXwnc", "another one https://www.google.com/ to try")
+
+remove_url(x)
+
+
+
